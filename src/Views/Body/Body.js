e6fda0272a69f132166697201735e4ff2e611992
Preshen Naidoo <Preshen@easygames.co.za>
2023-11-22 07:35:42 +0200

    Case Studies

--- src/Views/Body/Body.js
+++ src/Views/Body/Body.js
@@ -1,10 +1,12 @@
 import './Body.scoped.css';
 import About from './AboutUs/About';
+import Case from './CaseStudies/Case';
 
 function Body(){
     return (
         <div className='body-container'>
             <About></About>
+            <Case></Case>
         </div>
     )
 }
--- src/Views/Body/CaseStudies/Case.js
+++ src/Views/Body/CaseStudies/Case.js
@@ -1,9 +1,39 @@
 import './Case.scoped.css';
+import rectangle from '../../../Assets/Rectangle 1.svg';
 
 function Case() {
     return (
-        <div>
-            Case
+        <div className='case-container'>
+            <div className='case-header'>
+                <img src={rectangle} alt="rectangle"></img>
+                <p>Case Studies</p>
+            </div>
+
+            <div className='case-slider'>
+                <div className='olympian'>
+                    <div>
+                    <img src={rectangle} alt="rectangle"></img>
+                    <p>The Olympian</p>
+                    <p>The only athlete in the world to do her Olympic routine in 2020.</p>
+                    </div>
+                </div>
+
+                <div className='dragon'>
+                    <div>
+                        <img src={rectangle} alt="rectangle"></img>
+                        <p>The Savings Jar</p>
+                        <p>Grow your savings treasure and grow your dragon.</p>
+                    </div>
+                </div>
+
+                <div className='mali'>
+                    <div>
+                        <img src={rectangle} alt="rectangle"></img>
+                        <p>Skhokho seMali</p>
+                        <p>Helping South Africans become #CashCleva with Skhokho and TymeBank.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
